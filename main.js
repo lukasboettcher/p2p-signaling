cf9e1b34781833abd86d38510e5b7cadbd52900a
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-03-01 05:14:38 +0100

    create custom websocket server implementation

--- main.js
+++ main.js
@@ -1,108 +1,136 @@
-const io = require("socket.io")(3000);
+import { randomUUID } from "crypto";
+import WebSocket, { WebSocketServer } from 'ws';
 
-io.of("/screen").on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).emit('user-connected', userId)
+const wss = new WebSocketServer({ port: 3000, clientTracking: true });
 
-        socket.on("offer", (to, from, signal) => {
-            io.of("/screen").to(to).emit("offer", from, signal)
-        })
+// function handleProtocols(protocols, request) {
+//     console.log(protocols);
+//     // console.log(request);
+// }
 
-        socket.on("answer", (to, from, signal) => {
-            io.of("/screen").to(to).emit("answer", from, signal)
-        })
+function heartbeat() {
+    this.isAlive = true;
+}
 
-        socket.on("ice-candidate", (to, from, candidate) => {
-            io.of("/screen").to(to).emit("ice-candidate", from, candidate)
-        })
-
-        socket.on("stop-stream", () => {
-            socket.to(roomId).emit("stop-stream", socket.id)
-        })
-
-        socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId)
-        })
-    })
-})
-
-const registry = {}
-
-io.of('/files').on('connect', socket => {
-    // console.log('client connected with id: ', socket.id)
-    socket.on('disconnect', () => {
-        // console.log('client disconnected with id: ', socket.id)
-        delete registry[socket.id]
-        io.of('/files').emit('users', registry)
-    })
-
-    socket.on('register', (name) => {
-        registry[socket.id] = { id: socket.id, name: name }
-        // console.log('user registered with id: ', socket.id)
-        // console.log(registry)
-        io.of('/files').emit('users', registry)
-    })
-
-    socket.on('offer', (to, offer) => {
-        // console.log("forwading offer");
-        io.of('/files').to(to).emit('offer', socket.id, offer)
-    })
-
-    socket.on('answer', (to, answer) => {
-        // console.log("forwading answer");
-        io.of('/files').to(to).emit('answer', socket.id, answer)
-    })
-
-    socket.on('candidate', (to, candidate) => {
-        // console.log("forwading candidate");
-        io.of('/files').to(to).emit('candidate', socket.id, candidate)
-    })
-
-})
+const pingInterval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 30000);
 
 const CANVAS_DATA = new Object();
+const allowedEntries = ['to', 'answer', 'offer', 'join', 'ice', 'stop', 'hello', 'msg']
 
-io.of("/draw").on("connection", (socket) => {
+wss.on('connection', function connection(ws) {
+    ws.on('pong', heartbeat);
+    ws.rooms = new Set();
+    ws.id = randomUUID();
+    ws.send(JSON.stringify({ id: ws.id }));
+    ws.on('close', () => broadcast(ws, { disconnected: ws.id }));
 
-    socket.on('join-room', (roomId) => {
-        if (!CANVAS_DATA[roomId]) {
-            CANVAS_DATA[roomId] = { lines: [], texts: [] };
-            // remove the 
-            setTimeout(() => {
-                io.of("/draw").in(roomId).emit('clear');
-                delete CANVAS_DATA[roomId];
-                console.warn("Removing the canvas room with id ", roomId, "after one day.");
-            },
-                20 * 24 * 60 * 60 * 1000
-            )
-        }
+    if (ws.protocol === 'draw') {
+        ws.on('message', function message(rawData, isBinary) {
+            let data;
+            try {
+                data = JSON.parse(rawData);
+                data = Object.fromEntries(Object.entries(data).filter(([k, v]) => v != null && ['join', 'leave', 'line', 'clear', 'pop'].includes(k)));
+            } catch (error) {
+                ws.send('Error parsing JSON, terminating!')
+                ws.terminate();
+                return;
+            }
+            if (data.join) {
+                if (!CANVAS_DATA[data.join]) {
+                    CANVAS_DATA[data.join] = { lines: [], texts: [] };
+                    // remove the 
+                    setTimeout(() => {
+                        delete CANVAS_DATA[data.join];
+                        console.warn("Removing the canvas room with id ", data.join, "after five day.");
+                    },
+                        5 * 24 * 60 * 60 * 1000
+                    )
+                }
+                ws.rooms = new Set([data.join]);
+                ws.room = data.join;
+                ws.send(JSON.stringify({ line: CANVAS_DATA[data.join] }))
+            }
+            if (data.line) {
+                CANVAS_DATA[ws.room].lines.push(data.line)
+                data.line = CANVAS_DATA[ws.room]
+            }
+            if (data.clear) {
+                CANVAS_DATA[data.clear].lines = [];
+                CANVAS_DATA[data.clear].texts = [];
+            }
+            if (data.pop) {
+                CANVAS_DATA[data.pop].lines.pop();
+                wss.clients.forEach(function each(client) {
+                    if (client.readyState === WebSocket.OPEN && client.protocol === ws.protocol && intersect(ws.rooms, client.rooms)) {
+                        client.send(JSON.stringify({ clear: 'pop', line: CANVAS_DATA[data.pop] }));
+                    }
+                });
+            }
 
-        socket.join(roomId)
+            broadcast(ws, data);
+        });
 
-        socket.emit('add-line', CANVAS_DATA[roomId])
-        socket.on('add-line', line => {
-            CANVAS_DATA[roomId].lines.push(line)
-            socket.to(roomId).emit('add-line', CANVAS_DATA[roomId])
-        })
-        socket.on('clear', () => {
-            CANVAS_DATA[roomId].lines = [];
-            CANVAS_DATA[roomId].texts = [];
-            socket.to(roomId).emit('clear')
-        })
-        socket.on('leave-room', () => {
-            socket.leave(roomId)
-        })
-        socket.on('pop', () => {
-            CANVAS_DATA[roomId].lines.pop();
-            io.of("/draw").in(roomId).emit('clear');
-            io.of("/draw").in(roomId).emit('add-line', CANVAS_DATA[roomId]);
-        })
-    })
+    } else if (['chat', 'screen', 'files'].includes(ws.protocol)) {
+        ws.on('message', function message(rawData, isBinary) {
+            let data;
+            try {
+                data = JSON.parse(rawData);
+                data = Object.fromEntries(Object.entries(data).filter(([k, v]) => v != null && allowedEntries.includes(k)));
+            } catch (error) {
+                ws.send('Error parsing JSON, terminating!')
+                ws.terminate();
+                return;
+            }
+            if (data.join) {
+                ws.rooms.add(data.join);
+                ws.send(JSON.stringify({ users: getUsers(ws) }))
+            }
+
+            broadcast(ws, data);
+        });
+    } else {
+        console.warn('Closed Socket, unknown Protocol!');
+        ws.send('Closed Socket, unknown Protocol!');
+        ws.close();
+    }
+
+
+
 });
 
-io.of("/chat").on("connection", (socket) => {
-    socket.on('msg', text => {
-        socket.broadcast.emit('msg', text)
-    })
-})
+wss.on('close', function close() {
+    clearInterval(pingInterval);
+});
+
+function broadcast(ws, d) {
+    const data = { ...d, from: ws.id, users: getUsers(ws) };
+    wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN && client.protocol === ws.protocol && intersect(ws.rooms, client.rooms)) {
+            if (data.to) {
+                if (client.id === data.to) {
+                    client.send(JSON.stringify(data));
+                }
+            } else {
+                client.send(JSON.stringify(data));
+            }
+        }
+    });
+}
+
+function intersect(a, b) {
+    return new Set([...a].filter(i => b.has(i))).size > 0;
+}
+
+function getCount(ws) {
+    return [...wss.clients].filter(c => c.protocol === ws.protocol && intersect(c.rooms, ws.rooms)).length;
+}
+
+function getUsers(ws) {
+    return [...wss.clients].filter(c => c.protocol === ws.protocol && intersect(c.rooms, ws.rooms)).map(c => c.id);
+}
